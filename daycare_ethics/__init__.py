412d06a1591287a73121545faa0274dd4bfe29c9
Julian Gonggrijp <j.gonggrijp@uu.nl>
2014-12-17 16:22:26 +0100

    Remove spurious imports

--- daycare_ethics/__init__.py
+++ daycare_ethics/__init__.py
@@ -7,8 +7,8 @@
 
 from flask import Flask
 
-from .database import db, models
-from .server import public, views
+from .database import db
+from .server import public
 
 def create_app (config_file=None, config_obj=None):
     app = Flask(__name__, static_folder = 'www', static_url_path = '')
