5271596f284cc2b8d8a4084905346e43f7b73202
Julian Gonggrijp <j.gonggrijp@uu.nl>
2015-02-04 10:28:50 +0100

    Remove obsolete picture reference tests

--- daycare_ethics/tests/test_database/test_models.py
+++ daycare_ethics/tests/test_database/test_models.py
@@ -12,11 +12,9 @@ from ...database.models import *
 class SQLADeclaredAttrTestCase(TestCase):
     def test_picture_refs(self):
         self.assertIn('picture', Case.__dict__)
-        self.assertIn('picture', BrainTeaser.__dict__)
 
     def test_picture_backrefs(self):
         self.assertIn('cases', Picture.__dict__)
-        self.assertIn('brain_teasers', Picture.__dict__)
 
 
 class ModelsAreCreatedTestCase(BaseFixture):
