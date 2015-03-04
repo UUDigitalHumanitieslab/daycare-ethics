2ab5f534e7fe3f740fd7dc5169156423201e1bfc
Julian Gonggrijp <j.gonggrijp@uu.nl>
2015-03-04 16:57:31 +0100

    Start writing tests for casus client side

--- daycare_ethics/www/spec/index.js
+++ daycare_ethics/www/spec/index.js
@@ -11,10 +11,44 @@ describe('app', function() {
         };
         beforeEach(function(done) {
             spyOn(app, 'onDeviceReady');
+            spyOn(app, 'preloadContent');
             emulateDeviceReady(done);
         });
         it('should bind deviceready', function() {
             expect(app.onDeviceReady).toHaveBeenCalled();
+        });
+        it('should preload content', function() {
+            expect(app.preloadContent).toHaveBeenCalled();
+        });
+    });
+    
+    describe('preloadContent', function() {
+        function fakeJQueryGet (url, data, success, dataType) {
+            if (url === '/case/') success({
+                'id': 1,
+                'title': 'testcasus',
+                'publication': '2015-03-02',
+                'week': '10',
+                'closure': null,
+                'text': 'some dummy text',
+                'proposition': 'difficult question',
+                'picture': null,
+                'background': null
+            }, '200 OK');
+        };
+        beforeEach(function() {
+            $('#stage').html('<div id="plate">' +
+                '<h3>Casus van week <span class="week-number"></span></h3>' +
+                '<div id="case-display" style="width: 18em; height: 12em; background: #808080"></div>' +
+                '<p id="case-text"></p>' +
+                '<p id="case-proposition"></p>' +
+                '<a href="#" class="ui-btn ui-btn-inline ui-corner-all">Ja</a>' +
+                '<a href="#" class="ui-btn ui-btn-inline ui-corner-all">Nee</a>' +
+                '</div>');
+            spyOn($, 'get').and.callFake(fakeJQueryGet);
+        });
+        it('must prepopulate the HTML content with casus data', function() {
+            expect($('#plate .week-number').html()).toBe('10');
         });
     });
 
