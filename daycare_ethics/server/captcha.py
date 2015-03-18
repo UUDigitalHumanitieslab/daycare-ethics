de6af9d123a218f4faa6806ef77258bade83c228
Julian Gonggrijp <j.gonggrijp@uu.nl>
2015-03-18 17:30:44 +0100

    Start collecting captcha terms

---
+++ daycare_ethics/server/captcha.py
@@ -0,0 +1,75 @@
+# (c) 2014 Digital Humanities Lab, Faculty of Humanities, Utrecht University
+# Author: Julian Gonggrijp, j.gonggrijp@uu.nl
+
+"""
+    Semantic CAPTCHA data for the Dutch language.
+"""
+
+
+FLOWER = set('''
+    roos madelief margriet zonnebloem sneeuwklokje gadiool geranium gentiaan
+    sneeuwklokje tulp chrysant korenbloem krokus lelie iris narcis hyacinth
+    anemoon klaproos klokje leeuwenbekje paardenbloem cyclaam jasmijn vlier
+'''.strip().split())
+
+
+VEGETABLE = set('''
+    kool spitskool sla aubergine courgette sperzieboon tomaat komkommer
+    rodekool bloemkool broccoli spruitje doperwt andijvie postelijn spinazie
+    boon peultje boerenkool koolraap koolrabi meiraap rapesteel wortel peen
+    winterpeen bospeen radijs bleekselderij knolselderij biet rucola linze
+    tuinboon erwt kikkererwt ui avocado
+'''.strip().split())
+
+
+FRUIT = set('''
+    appel peer banaan sinaasappel mandarijn citroen tomaat limoen meloen mango
+    passiefruit bosbes aardbei druif ananas framboos braam sterfruit grapefruit
+    kiwi veenbes cranberry abrikoos pruim avocado bloedsinaasappel vijg dadel
+    granaatappel kers nectarine perzik papaja pompelmoes rozenbottel vlierbes
+'''.strip().split())
+
+
+FISH = set('''
+    snoek kabeljauw haring karper haai vliegvis rog goudvis maanvis makreel
+    guppy forel zalm tong paling koolvis tilapia pangasius sardine tonijn baars
+    pijlstaartrog rivierharing ansjovis steenvis zaaghaai hamerhaai lipvis voorn
+    lantaarnvis koffervis bot brasem tandbaars tandkarper sidderrog egelvis
+    zeewolf heilbot steur zwaardvis schelvis schol sidderrog kogelvis zeepaardje
+    zeesnoek meerval
+'''.strip().split())
+
+
+ = set('''
+    
+'''.strip().split())
+
+
+ = set('''
+    
+'''.strip().split())
+
+
+ = set('''
+    
+'''.strip().split())
+
+
+ = set('''
+    
+'''.strip().split())
+
+
+ = set('''
+    
+'''.strip().split())
+
+
+ = set('''
+    
+'''.strip().split())
+
+
+ = set('''
+    
+'''.strip().split())
