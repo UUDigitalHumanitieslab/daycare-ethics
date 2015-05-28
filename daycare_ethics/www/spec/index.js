35aad0a5bc2a4bd49d697b1c07588a6f3c236b06
Julian Gonggrijp <j.gonggrijp@uu.nl>
2015-05-28 14:20:41 +0200

    Add specs for app.loadReflection

--- daycare_ethics/www/spec/index.js
+++ daycare_ethics/www/spec/index.js
@@ -14,6 +14,32 @@ describe('app', function() {
         'picture': null,
         'background': '#998877'
     };
+    var fakeReflectionData = {
+        'id': 2,
+        'title': 'testreflection',
+        'publication': '2015-03-02',
+        'week': '10',
+        'closure': null,
+        'text': 'some dummy text',
+        'responses': [
+            {
+                'id': 1,
+                'up': 0,
+                'down': 15,
+                'submission': '2015-03-02',
+                'pseudonym': 'malbolge',
+                'message': '&lt;script&gt;wreakHavoc();&lt;/script&gt;'
+            },
+            {
+                'id': 2,
+                'up': 1,
+                'down': 10,
+                'submission': '2015-03-03',
+                'pseudonym': 'victim',
+                'message': 'help me'
+            }
+        ]
+    };
     
     beforeEach(function() {
         app.scope = $('#stage');
@@ -116,6 +142,48 @@ describe('app', function() {
             expect($('#plate .case-text')).toContainText('some dummy text');
             expect($('#plate .case-proposition')).toContainText('difficult question');
             expect($('#plate')).toHaveCss({'background-color': 'rgb(153, 136, 119)'});
+        });
+    });
+    
+    describe('loadReflection', function() {
+        beforeEach(function() {
+            app.insertPages();
+            this.date = $.now();
+        });
+        it('populates a page with reflection data', function() {
+            spyOn(app, 'appendReply').and.callThrough();
+            app.loadReflection($('#mirror'), fakeReflectionData);
+            expect($('#mirror .week-number')).toContainText('10');
+            expect($('#mirror .reflection-text')).toContainText('some dummy text');
+            expect(app.appendReply.calls.count()).toBe(2);
+            expect($('#mirror .reflection-discussion > *')).toHaveLength(2);
+            $('#stage, #mirror').show();
+            expect($('#mirror .reflection-response')).toBeVisible();
+            expect($('#mirror .reflection-closed-notice')).toBeHidden();
+            expect($('#mirror .reflection-closure-announce')).toBeHidden();
+        });
+        it('warns the user if a closure date is set', function() {
+            this.date += 24 * 60 * 60 * 1000; // one day ahead
+            var date = (new Date(this.date)).toISOString().slice(0, 10)
+            var customFakeData = _(fakeReflectionData).clone();
+            customFakeData.closure = date;
+            app.loadReflection($('#mirror'), customFakeData);
+            $('#stage, #mirror').show();
+            expect($('#mirror .reflection-response')).toBeVisible();
+            expect($('#mirror .reflection-closed-notice')).toBeHidden();
+            expect($('#mirror .reflection-closure-announce')).toBeVisible();
+            expect($('#mirror .reflection-closure-date')).toContainText(date);
+        });
+        it('warns the user when replying is no longer possible', function() {
+            this.date -= 24 * 60 * 60 * 1000; // one day prior
+            var date = (new Date(this.date)).toISOString().slice(0, 10)
+            var customFakeData = _(fakeReflectionData).clone();
+            customFakeData.closure = date;
+            app.loadReflection($('#mirror'), customFakeData);
+            $('#stage, #mirror').show();
+            expect($('#mirror .reflection-response')).toBeHidden();
+            expect($('#mirror .reflection-closed-notice')).toBeVisible();
+            expect($('#mirror .reflection-closure-announce')).toBeHidden();
         });
     });
 
