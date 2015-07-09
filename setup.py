1ef2a25ed4efeed0332eafe1f48dc1391ecfd235
Julian Gonggrijp <j.gonggrijp@uu.nl>
2015-07-09 15:35:13 +0200

    Bump the version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name = "daycare_ethics",
-    version = "0.3.2",
+    version = "0.4.0",
     packages = find_packages(),
     package_data = {
         'daycare_ethics': [
