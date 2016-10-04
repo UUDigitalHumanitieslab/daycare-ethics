c4aedd53fdd5b17781ed609abfed6ca1ff881ab6
Julian Gonggrijp <j.gonggrijp@uu.nl>
2016-10-04 17:44:34 +0200

    Add sanity checks/regression tests for session management
    I hoped that these unit tests would reproduce an error, which
    prevents some information from being fetched the mobile application
    on first load. The current reflection is always displayed but the
    case and/or the tips tend to be missing (issue #6). The server log
    contained the following for an instance of this problem:
    
    IntegrityError: (_mysql_exceptions.IntegrityError) (1062, "Duplicate entry '0K3KdtHs7sy4ZZUpXL5ox3VCFXzomr' for key 'PRIMARY'") [SQL: u'INSERT INTO session (token, expires, payload) VALUES (%s, %s, %s)'] [parameters: ('0K3KdtHs7sy4ZZUpXL5ox3VCFXzomr', datetime.datetime(2016, 11, 4, 10, 12, 17, 576319), '\x80\x02}q\x01(U\x05tokenq\x02X\x1e\x00\x00\x000K3KdtHs7sy4ZZUpXL5ox3VCFXzomrq\x03U\x07taintedq\x04\x88U\n_permanentq\x05\x88u.')]
    
    Which indicates that the information was not fetched because the
    server returned a 5xx response, which it did because it considered
    the session invalid. It tried to store the invalid session as
    "tainted" and then stumbled across a duplicate, because the token was
    actually valid and pre-existing.
    
    The tests in this commit do not reproduce the problem. However, while
    writing this, I realise the problem is caused by a race condition.
    This is something to fix in the next commit.

--- daycare_ethics/tests/test_server/test_session.py
+++ daycare_ethics/tests/test_server/test_session.py
@@ -1,12 +1,14 @@
-# (c) 2014, 2015 Digital Humanities Lab, Utrecht University
+# (c) 2014, 2015, 2016 Digital Humanities Lab, Utrecht University
 # Author: Julian Gonggrijp, j.gonggrijp@uu.nl
 
 from unittest import TestCase
 
+from ..common_fixtures import BaseFixture
 from ...server.session import *
+from ...database.models import Session
 
 
-class BasicsTestCase (TestCase):
+class BasicsTestCase(TestCase):
     def test_generate_key(self):
         keys = [generate_key() for i in range(10)]
         
@@ -18,3 +20,55 @@ class BasicsTestCase (TestCase):
             self.assertRegexpMatches(k, '[a-zA-Z0-9]{30}')
             self.assertEqual(len(k), KEY_LENGTH)
 
+
+class SessionInterfaceTestCase(BaseFixture):
+    def setUp(self):
+        super(SessionInterfaceTestCase, self).setUp()
+        self.cookie_name = self.app.config['SESSION_COOKIE_NAME']
+    
+    def test_nomodify(self):
+        # Run 1: start from scratch, no preexisting session
+        with self.client.session_transaction():
+            pass
+        with self.request_context():
+            sessions = Session.query.all()
+        self.assertEqual(len(sessions), 1)
+        token = sessions[0].token
+        payload = sessions[0].payload
+        
+        # Run 2: send the same token, explicitly
+        with self.client.session_transaction(data={'t': token}):
+            pass
+        with self.request_context():
+            sessions = Session.query.all()
+        self.assertEqual(len(sessions), 1)
+        self.assertEqual(sessions[0].token, token)
+        self.assertEqual(sessions[0].payload, payload)
+        
+        # Run 3: send the same token, by cookie
+        with self.client.session_transaction(headers={
+            'Cookie': '{}={};'.format(self.cookie_name, token),
+        }):
+            pass
+        with self.request_context():
+            sessions = Session.query.all()
+        self.assertEqual(len(sessions), 1)
+        self.assertEqual(sessions[0].token, token)
+        self.assertEqual(sessions[0].payload, payload)
+        
+        # Run 4: send a faux token, explicitly
+        faux_token = ''.join(reversed(token))
+        with self.client.session_transaction(data={'t': faux_token}) as s:
+            self.assertEqual(s['token'], faux_token)
+            self.assertTrue(s['tainted'])
+            self.assertTrue(s.permanent)
+        with self.request_context():
+            sessions = Session.query.all()
+            sacred_session = Session.query.get(token)
+            corrupted_session = Session.query.get(faux_token)
+        self.assertEqual(len(sessions), 2)
+        self.assertIsNotNone(sacred_session)
+        self.assertIsNotNone(corrupted_session)
+        self.assertEqual(sacred_session.payload, payload)
+        self.assertTrue('tainted' in corrupted_session.payload)
+        self.assertTrue(corrupted_session.payload['tainted'])
